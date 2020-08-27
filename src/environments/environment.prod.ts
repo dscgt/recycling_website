a1623c5180fdd799f3301c13f022fbb25e61c620
Michael Fan <michaelyfan100@gmail.com>
2020-08-27 00:23:40 -0400

    Remove .firebaserc, update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,23 +1,13 @@
 export const environment = {
   production: true,
-  firebaseConfigRoutes: {
-    apiKey: "AIzaSyCHz8Yz0SigXL5xOioF_l2UzYpkrkdhC7Q",
-    authDomain: "route-recorder-de136.firebaseapp.com",
-    databaseURL: "https://route-recorder-de136.firebaseio.com",
-    projectId: "route-recorder-de136",
-    storageBucket: "route-recorder-de136.appspot.com",
-    messagingSenderId: "53721343327",
-    appId: "1:53721343327:web:f872ccfcc666c82fc7c2ea",
-    measurementId: "G-6K8HWZ5GME"
-  },
-  firebaseConfigCheckin: {
-    apiKey: "AIzaSyALRmV3vAIOVrHgMnDlxqqwNjHpi7znJwA",
-    authDomain: "recycling-checkin.firebaseapp.com",
-    databaseURL: "https://recycling-checkin.firebaseio.com",
-    projectId: "recycling-checkin",
-    storageBucket: "recycling-checkin.appspot.com",
-    messagingSenderId: "872084182292",
-    appId: "1:872084182292:web:e1662524c924c46c4f1e01",
-    measurementId: "G-MER4YF3R63"
+  firebase: {
+    apiKey: "AIzaSyCYftHs5qR8ma2bEpT8NoKdE4lUyMHmbss",
+    authDomain: "gt-recycling.firebaseapp.com",
+    databaseURL: "https://gt-recycling.firebaseio.com",
+    projectId: "gt-recycling",
+    storageBucket: "gt-recycling.appspot.com",
+    messagingSenderId: "619187120497",
+    appId: "1:619187120497:web:1b419f3ad094e14458eed0",
+    measurementId: "G-SC438D4WT5"
   }
 };
