1ee3d801f0ad02ab8ba3d234a693740e880f1587
2019aliu <aliu338@gatech.edu>
2020-08-30 23:59:45 -0400

    download excel file route working - no error checking :/ be careful

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const xlsx = require('xlsx');
 const FileSaver = require('file-saver');
-// const Blob = require('blob');
 
 const admin = require("firebase-admin");
 admin.initializeApp();
@@ -31,7 +30,7 @@ exports.seedRouteData = functions.https.onRequest(async (req, res) => {
         }
       ],
       "modelId": "modelId_here",
-      "modelTitle": "modelTitle_here",
+      "modelTitle": "modelTitle numero dos",
     });
 
     res.json({ result: `Message with ID: ${writeResult.id} added.` });
@@ -53,11 +52,30 @@ Format of the sheet:
 title	startime	endtime	property1	property2	property3	stop0	stop0_property1	stop0_property2	stop0property3	stop1title	stop1property1	stop1property2
 */
 
-// sauce: https://redstapler.co/sheetjs-tutorial-create-xlsx/
+const emptyCellValue = "N/A";
+const propertyStringFormat = (key, value) => `${key}: ${value}`;
+const propertiesListFormatter = (obj, length) => {
+  let ret = Object.entries(obj).map((entry) => propertyStringFormat(entry[0], entry[1]));
+  const missingEntries = length - Object.keys(obj).length
+  for (let i = 0; i < missingEntries; i++) {
+    ret.push(emptyCellValue);
+  }
+  return ret;
+}
+
+/**
+ * Function that generates an excel sheet for the records databases
+ * Query params:
+ * filename - the filename of the file to download
+ * sheetTitle - the title of the Excel sheet
+ * recordType - name of the firebase collection of records. 
+ */
 exports.generateExcelSheet = functions.https.onRequest(async (req, res) => {
   // initialize sheet
+  // sauce: https://redstapler.co/sheetjs-tutorial-create-xlsx/
   const workbookTitle = req.query.filename;
   const sheetTitle = req.query.sheetTitle;
+  const collectionName = `${req.query.recordType.toLowerCase()}_records`
   const wb = xlsx.utils.book_new();
   wb.Props = {
     Title: workbookTitle
@@ -67,7 +85,7 @@ exports.generateExcelSheet = functions.https.onRequest(async (req, res) => {
   // read data from Fiyerrrbaaaaaaasssse
   let data;
   try {
-    let snapshot = await db.collection("route_records").get();
+    let snapshot = await db.collection(collectionName).get();
     data = snapshot.docs.map(doc => doc.data());
   } catch (e) {
     console.log('ERROR');
@@ -86,51 +104,58 @@ exports.generateExcelSheet = functions.https.onRequest(async (req, res) => {
   let ws_data = [];
 
   // preprocessing - find the maximum number of properties for all records and for each stop
-  let generalProperties = new Set();
-  let stopProperties = [];
+  let maxGeneralProperties = 0;
+  let maxStopProperties = [];
 
-  data.map((record) => {
-    record.properties.forEach((value) => generalProperties.add(value));
+  data.forEach((record) => {
+    // Add in all of the properties for the record
+    maxGeneralProperties = Math.max(maxGeneralProperties, Object.keys(record.properties).length);
+    // Add in all properties for the particular stop numbers
     for (let i = 0; i < record.stops.length; i++) {
-      let stopPropertiesLength = Object.values(record.stops[i].properties).length;
       if (i >= maxStopProperties.length) {
-        maxStopProperties.push(stopPropertiesLength);
+        maxStopProperties.push(Object.keys(record.stops[i].properties).length);
       } else {
-        maxStopProperties[i] = max(maxStopProperties[i], stopPropertiesLength);
+        maxStopProperties[i] = Object.keys(record.stops[i].properties).length;
       }
     }
   })
 
-  generalProperties = Array.from(generalProperties);
-
-  const headers = [];
+  // Add in headers
+  const headers = ["Title", "Checkout Time", "Checkin Time"];
+  for (let i = 0; i < maxGeneralProperties; i++) {
+    headers.push(`Property ${i}`);
+  }
+  maxStopProperties.forEach((maxStopProps, index) => {
+    headers.push(`Stop ${index}`);
+    for (let i = 0; i < maxStopProps; i++) {
+      headers.push(`Stop ${index}: Property ${i}`);
+    }
+  })
   ws_data.push(headers);
 
-  data.map((record) => {
-    const properties = Object.values(record.properties)
+  // Add in the rows of data
+  data.forEach((record) => {
+    const properties = propertiesListFormatter(record.properties, maxGeneralProperties);
     let stops = []
     for (let i = 0; i < record.stops.length; i++) {
-      let stops[i] =
+      stops.push(record.stops[i].title);
+      stops = stops.concat(propertiesListFormatter(record.stops[i].properties, maxStopProperties[i]));
     }
     let row = [
       record.modelTitle,
       record.startTime,
       record.endTime,
     ];
-    row.concat(properties);
-    ws_data.push();
+    row = row.concat(properties);
+    row = row.concat(stops);
+    ws_data.push(row);
   });
 
+  // Convert array of array (aoa) to excel sheet and full senddddddd
   let ws = xlsx.utils.aoa_to_sheet(ws_data);
   wb.Sheets[sheetTitle] = ws;
 
   let wbout = xlsx.write(wb, {bookType: 'xlsx', type: 'buffer'});
+  res.type('blob');
   res.status(200).send(wbout);
 })
-
-function s2ab(s) {
-  let buf = new ArrayBuffer(s.length);
-  let view = new Uint8Array(buf);
-  for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
-  return buf;
-}
